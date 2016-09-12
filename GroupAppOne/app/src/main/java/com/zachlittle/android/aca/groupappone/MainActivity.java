7adc537e4fd9f1c2cfb5f548fef5de60bf03d39b
skywalker22 <jacksonmmix@gmail.com>
2016-09-12 14:51:06 -0500

    yo x 3

--- GroupAppOne/app/src/main/java/com/zachlittle/android/aca/groupappone/MainActivity.java
+++ GroupAppOne/app/src/main/java/com/zachlittle/android/aca/groupappone/MainActivity.java
@@ -1,13 +1,38 @@
-package com.zachlittle.android.aca.groupappone;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+package com.jacksonmix.android.aca.groupappone;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.widget.Button;
+        import android.widget.EditText;
+
+        public class MainActivity extends AppCompatActivity {
+
+            EditText mName;
+            EditText mAddress;
+            EditText mPhoneNumber;
+            Button mButtonSubmit;
+
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(com.jacksonmix.android.aca.groupappone.R.layout.activity_main);
+
+                mName = (EditText)findViewById(com.jacksonmix.android.aca.groupappone.R.id.);
+                mPhoneNumber = (EditText)findViewById(com.jacksonmix.android.aca.groupappone.R.id.);
+                mAddress = (EditText)findViewById(com.jacksonmix.android.aca.groupappone.R.id.);
+                mButtonSubmit = (Button)findViewById(com.jacksonmix.android.aca.groupappone.R.id.sumbitButton);
+
+                mButtonSumbit.setOnClickListener();
+
+                String nameInput = mName.getText().toString();
+                String phoneNumberInput;
+                String addressInput;
+
+
+
+            }
+        }
+

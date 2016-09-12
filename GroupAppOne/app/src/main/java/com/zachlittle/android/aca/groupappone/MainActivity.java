ef8152bd20f5fdad678c96cc425c681db1b001e8
mwilson03 <hogman_19_98@yahoo.com>
2016-09-12 14:15:23 -0500

    add intent method and TAG variables

--- GroupAppOne/app/src/main/java/com/zachlittle/android/aca/groupappone/MainActivity.java
+++ GroupAppOne/app/src/main/java/com/zachlittle/android/aca/groupappone/MainActivity.java
@@ -1,13 +1,26 @@
 package com.zachlittle.android.aca.groupappone;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG_NAME = "com.zachlittle.android.aca.NAME";
+    public static final String TAG_PHONE = "com.zachlittle.android.aca.PHONE";
+    public static final String TAG_ADDRESS = "com.zachlittle.android.aca.ADDRESS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    public void goToResults() {
+        Intent intent = new Intent(this.Voldiescontact.class);
+        intent.putStringExtra(TAG_NAME, nameInput);
+        intent.putStringExtra(TAG_PHONE, phoneNumberInput);
+        intent.putStringExtra(TAG_ADDRESS, addressIntent);
     }
 }

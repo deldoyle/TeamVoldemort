304a06d7bbc0214306072255f13ee0c84a2c9e27
DanielA <obsidian151.da@gmail.com>
2016-09-12 15:49:56 -0500

    map butt

--- GroupAppOne/app/src/main/java/com/zachlittle/android/aca/groupappone/VoldiesContact.java
+++ GroupAppOne/app/src/main/java/com/zachlittle/android/aca/groupappone/VoldiesContact.java
@@ -3,7 +3,6 @@ package com.zachlittle.android.aca.groupappone;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -18,33 +17,33 @@ public class VoldiesContact extends AppCompatActivity{
     Button mButton;
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
 
         mTextView = (TextView) findViewById(R.id.resultsTextView);
 
         mButton = (Button) findViewById(R.id.googleButton);
 
-        mButton.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View view) {
-                Intent mapsIntent = null;
-                mapsIntent = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("geo: " + " "));
-
-            }
-        });
 
         Intent intent = getIntent();
         String nameFromMain = intent.getStringExtra(MainActivity.TAG_NAME);
         String phoneFromMain = intent.getStringExtra(MainActivity.TAG_PHONE);
-        String addressFromMain = intent.getStringExtra(MainActivity.TAG_ADDRESS);
+        final String addressFromMain = intent.getStringExtra(MainActivity.TAG_ADDRESS);
 
         mTextView.append(nameFromMain + "\n");
         mTextView.append(phoneFromMain + "\n");
         mTextView.append(addressFromMain + "\n");
 
+        mButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent mapsIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo: " + addressFromMain ));
+
+            }
+        });
 
     }
 }

cbcb667303b06d2765eca1557a79b01de561d039
James Little <jzachlittle@gmail.com>
2016-09-12 14:43:23 -0500

    bringing it together

--- GroupAppOne/app/src/main/java/com/zachlittle/android/aca/groupappone/VoldiesContact.java
+++ GroupAppOne/app/src/main/java/com/zachlittle/android/aca/groupappone/VoldiesContact.java
@@ -1,9 +1,12 @@
 package com.zachlittle.android.aca.groupappone;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -12,13 +15,27 @@ import android.widget.TextView;
 public class VoldiesContact extends AppCompatActivity{
 
     TextView mTextView;
-
+    Button mButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
 
         mTextView = (TextView) findViewById(R.id.);
+
+        mButton = (Button) findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent mapsIntent = null;
+                (R.id.imageButton3);
+                mapsIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("geo: " + " "));
+
+            }
+        });
 
         Intent intent = getIntent();
         String nameFromMain = intent.getStringExtra(MainActivity.TAG_NAME);

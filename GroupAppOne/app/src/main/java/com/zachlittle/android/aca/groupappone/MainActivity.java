af83e0398355994261a40060d63eca2d46d38d63
James Little <jzachlittle@gmail.com>
2016-09-12 14:56:27 -0500

    commit

--- GroupAppOne/app/src/main/java/com/zachlittle/android/aca/groupappone/MainActivity.java
+++ GroupAppOne/app/src/main/java/com/zachlittle/android/aca/groupappone/MainActivity.java
@@ -3,17 +3,34 @@ package com.zachlittle.android.aca.groupappone;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG_NAME = "com.zachlittle.android.aca.NAME";
     public static final String TAG_PHONE = "com.zachlittle.android.aca.PHONE";
     public static final String TAG_ADDRESS = "com.zachlittle.android.aca.ADDRESS";
+    EditText mName;
+    EditText mAddress;
+    EditText mPhoneNumber;
+    Button mButtonSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mName = (EditText)findViewById(com.jacksonmix.android.aca.groupappone.R.id.);
+        mPhoneNumber = (EditText)findViewById(com.jacksonmix.android.aca.groupappone.R.id.);
+        mAddress = (EditText)findViewById(com.jacksonmix.android.aca.groupappone.R.id.);
+        mButtonSubmit = (Button)findViewById(com.jacksonmix.android.aca.groupappone.R.id.sumbitButton);
+
+        mButtonSumbit.setOnClickListener();
+
+        String nameInput = mName.getText().toString();
+        String phoneNumberInput;
+        String addressInput;
 
 
     }

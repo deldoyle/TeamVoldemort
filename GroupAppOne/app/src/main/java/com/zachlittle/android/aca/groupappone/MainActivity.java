4b26db9c2f7482866a45ef7e8c54edd017f123a7
DanielA <obsidian151.da@gmail.com>
2016-09-12 14:20:25 -0500

    map

--- GroupAppOne/app/src/main/java/com/zachlittle/android/aca/groupappone/MainActivity.java
+++ GroupAppOne/app/src/main/java/com/zachlittle/android/aca/groupappone/MainActivity.java
@@ -1,7 +1,11 @@
 package com.zachlittle.android.aca.groupappone;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent mapsIntent = null;
+                (R.id.imageButton3);
+                mapsIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("geo: " + " "));
+
+            }
+        });
     }
 }

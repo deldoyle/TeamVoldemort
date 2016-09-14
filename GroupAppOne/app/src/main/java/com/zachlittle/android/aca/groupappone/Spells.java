ee13beeef81f24473aa9125819c65b3ee27a292f
James Little <jzachlittle@gmail.com>
2016-09-14 12:09:15 -0500

    fixed most bugs

--- GroupAppOne/app/src/main/java/com/zachlittle/android/aca/groupappone/Spells.java
+++ GroupAppOne/app/src/main/java/com/zachlittle/android/aca/groupappone/Spells.java
@@ -8,14 +8,18 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 
 public class Spells extends AppCompatActivity implements SensorEventListener {
 
     RadioGroup mRadioGroup;
-    private final static float ACC = 18;
-    MediaPlayer mp = new MediaPlayer();
+    RadioButton mSpellOne;
+    RadioButton mSpellTwo;
+    RadioButton mSpellThree;
+    private final static float ACC = 70;
+    MediaPlayer mp;
     int file = R.raw.alohomora;
 
     @Override
@@ -24,34 +28,38 @@ public class Spells extends AppCompatActivity implements SensorEventListener {
         setContentView(R.layout.magic_stuff);
 
         mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        mSpellOne = (RadioButton) findViewById(R.id.spellOne);
+        mSpellTwo = (RadioButton) findViewById(R.id.spellTwo);
+        mSpellThree = (RadioButton) findViewById(R.id.spellThree);
+
+        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int i) {
+                RadioButton rb = (RadioButton) group.findViewById(i);
+                switch (rb.getId()){
+                    case R.id.spellOne:
+                        file = R.raw.alohomora;
+                        Log.i("fileChange", "alohomora");
+                        break;
+                    case R.id.spellTwo:
+                        file = R.raw.threexpelliarmus;
+                        Log.i("fileChange", "expelliarmus");
+                        break;
+                    case R.id.spellThree:
+                        file = R.raw.obliviate;
+                        Log.i("fileChange", "obliviate");
+                        break;
+                }
+            }
+        });
 
         SensorManager shakeTheDevice = (SensorManager) getSystemService(SENSOR_SERVICE);
         shakeTheDevice.registerListener(this,
                 shakeTheDevice.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
                 SensorManager.SENSOR_DELAY_NORMAL);
 
-        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-
-                switch (mRadioGroup.getId()){
-                    case R.id.spellOne:
-                        file = R.raw.alohomora;
-                        break;
-                    case R.id.spellTwo:
-                        file = R.raw.threexpelliarmus;
-                        break;
-                    case R.id.spellThree:
-                        file = R.raw.obliviate;
-                        break;
-                }
-
-            }
-        });
-        mp = MediaPlayer.create(this, file);
 
     }
-
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
@@ -60,14 +68,12 @@ public class Spells extends AppCompatActivity implements SensorEventListener {
         if ((Math.abs(values[0]) > ACC || Math.abs(values[1]) > ACC || Math
                     .abs(values[2]) > ACC)) {
             Log.i("sensor", "running");
-            mp.start();
-        }
-        if(mp!=null) {
-            if(mp.isPlaying())
-                mp.stop();
-            mp.reset();
-            mp.release();
-            mp=null;
+            playThatFunkyMagic();
+            Log.i("sensor", "running after");
+
+            for (float i=0; i<200_000; i++) {
+
+            }
         }
 
     }
@@ -76,22 +82,13 @@ public class Spells extends AppCompatActivity implements SensorEventListener {
     public void onAccuracyChanged(Sensor sensor, int i) {
     }
 
-    /*private void playThatFunkyMagic(){
-        int file = R.raw.alohomora;
-        switch (spellID){
-            case 1:
-                file = R.raw.alohomora;
-                break;
-            case 2:
-                file = R.raw.threexpelliarmus;
-                break;
-            case 3:
-                file = R.raw.obliviate;
-                break;
-        }
+    private void playThatFunkyMagic(){
         mp = MediaPlayer.create(this, file);
         mp.start();
         mp.setLooping(false);
-
-    }*/
+        if(mp.isPlaying()) {
+        }else {
+            mp.release();
+        }
+    }
 }
